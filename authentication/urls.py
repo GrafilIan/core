176ac0c78e296ccdd17427daa4739859a4c234e7
GrafilIan <iancarlgrafil@sorsu.edu.ph>
2023-10-16 07:17:56 +0800

    Initial commit

---
+++ authentication/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from authentication import views
+
+urlpatterns = [
+    # login view from auth_views with custom login template
+    path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html',
+                                                # True means that if user is already logged in, it will redirect to homepage
+                                                redirect_authenticated_user=True), name='login'),
+
+    # logout view from auth_view
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # path for register view
+    path('register/', views.register, name='register'),
+
+    #path for activate view
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    #path to reset password
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html'), name='password_reset'),
+
+    #path to password_reset_done
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'), name='password_reset_done'),
+
+    #path to password_reset_confirm
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
+
+    #path to password reset complete
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
+
+
+    # path for homepage where successfull login will redirect
+    path('', views.homepage, name='homepage'),
+
+
+
+
+
+]
---
+++ authentication/views.py
@@ -0,0 +1,74 @@
+# import this to require login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+# import this for sending email to user
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from authentication.forms import UserRegistrationForm
+
+
+# Create your views here.
+
+@login_required(login_url='login')
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def register(request):
+    form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            # email user with activation link
+            current_site = get_current_site(request)
+            mail_subject = "Activate your account."
+
+            # the message will render what is written in authentication/email_activation/activate_email_message.html
+            message = render_to_string('authentication/email_activation/activate_email_message.html', {
+                    'user': form.cleaned_data['username'],
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token':  default_token_generator.make_token(user),
+                })
+            to_email = form.cleaned_data['email']
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            messages.success(request, 'Account created successfully. Please check your email to activate your account.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account creation failed. Please try again.')
+
+
+    return render(request, 'authentication/register.html',{
+        'form': form
+    })
+
+# to activate user from email
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return render(request, 'authentication/email_activation/activation_successful.html')
+    else:
+        return render(request, 'authentication/email_activation/activation_unsuccessful.html')
+
